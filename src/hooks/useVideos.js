a6120c211c39646f7e88da9aef4eb34decc3320d
orozakunov <92034948+orozakunov@users.noreply.github.com>
2022-04-11 16:42:20 +0600

    Add files via upload

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import youtube from "../apis/youtube";
+
+const useVideos = () => {
+    const [ videos, setVideos ] = useState([])
+    const [ selectedVideo, setSelectedVideo ] = useState(null)
+  
+  
+    const search = async (term) => {
+      const response = await youtube.get("/search", {
+        params: {
+          q: term,
+        },
+      })
+      setVideos(response.data.items)
+      setSelectedVideo(response.data.items[0])
+    }
+  
+    useEffect(() => {
+      search('siuuuu')
+    }, [])
+
+
+    return [ videos, search, selectedVideo, setSelectedVideo ]
+}
+
+export default useVideos
